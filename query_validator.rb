9d0480564846b9b4761cb5675b97df2060eb4f69
Luka Mirosevic <luka@goonbee.com>
2013-08-26 12:49:31 +0200

    -n Replaced tabs with spaces

--- query_validator.rb
+++ query_validator.rb
@@ -6,75 +6,74 @@
 # Created by Luka Mirosevic on 27/04/2013.
 # Copyright (c) 2013 Goonbee. All rights reserved.
 
-
 module Goonbee
-	module QueryValidator
-		class MalformedQueryError < StandardError
-		end
-		
-		class Validator
-			class << self
-				def type(obj)
-					case obj
-						when String
-							return :str
-						when Fixnum, Float, Bignum
-							return :num
-						when Hash
-							return :hash
-						when Array
-							return :array
-						when TrueClass, FalseClass
-							return :bool
-						when :_
-							return :any
-						else
-							return nil
-					end
-				end
+    module QueryValidator
+        class MalformedQueryError < StandardError
+        end
+        
+        class Validator
+            class << self
+                def type(obj)
+                    case obj
+                        when String
+                            return :str
+                        when Fixnum, Float, Bignum
+                            return :num
+                        when Hash
+                            return :hash
+                        when Array
+                            return :array
+                        when TrueClass, FalseClass
+                            return :bool
+                        when :_
+                            return :any
+                        else
+                            return nil
+                    end
+                end
 
-				def process(a, b)
-					if type(a) == :any
-						b
-					elsif type(a) == :str && type(b) == :str
-						b
-					elsif type(a) == :num && type(b) == :num
-						b
-					elsif type(a) == :bool && type(b) == :bool
-						b
-					elsif type(a) == :array && type(b) == :array
-						b.map {|i| process(a[0], i)}
-					elsif type(a) == :hash && type(b) == :hash
-						new_hash = Hash.new
+                def process(a, b)
+                    if type(a) == :any
+                        b
+                    elsif type(a) == :str && type(b) == :str
+                        b
+                    elsif type(a) == :num && type(b) == :num
+                        b
+                    elsif type(a) == :bool && type(b) == :bool
+                        b
+                    elsif type(a) == :array && type(b) == :array
+                        b.map {|i| process(a[0], i)}
+                    elsif type(a) == :hash && type(b) == :hash
+                        new_hash = Hash.new
 
-						a.each_key do |key|
-							if key[0] == '_'#optional key
-								search_key_s = key[1..-1]
-								search_key_sym = search_key_s.to_sym#this one is safe because it comes from our finite set query
+                        a.each_key do |key|
+                            if key[0] == '_'#optional key
+                                search_key_s = key[1..-1]
+                                search_key_sym = search_key_s.to_sym#this one is safe because it comes from our finite set query
 
-								optional = true
-							else#required key
-								search_key_s = key.to_s
-								search_key_sym = key
+                                optional = true
+                            else#required key
+                                search_key_s = key.to_s
+                                search_key_sym = key
 
-								optional = false
-							end
+                                optional = false
+                            end
 
-							if b.has_key?(search_key_s)#has string?
-								new_hash[search_key_sym] = process(a[key], b[search_key_s])
-							elsif b.has_key?(search_key_sym)#has symbol?
-								new_hash[search_key_sym] = process(a[key], b[search_key_sym])
-							else
-								raise MalformedQueryError unless optional
-							end
-						end
+                            if b.has_key?(search_key_s)#has string?
+                                new_hash[search_key_sym] = process(a[key], b[search_key_s])
+                            elsif b.has_key?(search_key_sym)#has symbol?
+                                new_hash[search_key_sym] = process(a[key], b[search_key_sym])
+                            else
+                                raise MalformedQueryError unless optional
+                            end
+                        end
 
-						return new_hash
-					else
-						raise MalformedQueryError
-					end
-				end
-			end
-		end
-	end
+                        return new_hash
+                    else
+                        raise MalformedQueryError
+                    end
+                end
+            end
+        end
+    end
 end
